43b12588106be62798844eb6f7ac4730b9f3fa96
ouwenxuan <ouwenxuan@senseauto.com>
2024-03-18 14:49:47 +0800

    setting up swap chain

--- main.cpp
+++ main.cpp
@@ -261,11 +261,18 @@ private:
 		// check queue families
 		QueueFamilyIndices indices = findQueueFamilies(device);
 		
-		// check swap chain support
+		// check required extension list
 		bool extensionsSupported = checkDeviceExtensionSupport(device);
 
-		return deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU &&
-			deviceFeatures.geometryShader && indices.isComplete() && extensionsSupported;
+		// check swap chain support, after extension support check
+		bool swapChainAdequate = false;
+		if(extensionsSupported) {
+			SwapChainSupportDetails swapChainSupport = querySwapChainSupport(device);
+			swapChainAdequate = !swapChainSupport.formats.empty() && !swapChainSupport.presentModes.empty();
+		}
+
+		return deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU 
+			&& indices.isComplete() && extensionsSupported && swapChainAdequate;
 	}
 
 	bool checkDeviceExtensionSupport(VkPhysicalDevice device) {
@@ -328,6 +335,65 @@ private:
 		
 		return indices;
 	}
+
+	// check swap chain support
+	struct SwapChainSupportDetails {
+		VkSurfaceCapabilitiesKHR capabilities;
+		std::vector<VkSurfaceFormatKHR> formats;
+		std::vector<VkPresentModeKHR> presentModes;
+	};
+
+	SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device) {
+		SwapChainSupportDetails details;
+
+		// get supported surface formats
+		uint32_t formatCount;
+		vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, &formatCount, nullptr);
+		if(formatCount != 0) {
+			// list of structs, resize to hold all available formats
+			details.formats.resize(formatCount);
+			vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, &formatCount, details.formats.data());
+		}
+
+		// get supported presentation modes
+		uint32_t presentModeCount;
+		vkGetPhysicalDeviceSurfacePresentModesKHR(device, surface, &presentModeCount, nullptr);
+		if(presentModeCount != 0) {
+			details.presentModes.resize(presentModeCount);
+			vkGetPhysicalDeviceSurfacePresentModesKHR(device, surface, &presentModeCount, details.presentModes.data());
+		}
+
+		return details;
+	}
+
+	// config swap chain
+	VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats) {
+		// use SRGB color format
+		for(const auto& availableFormat : availableFormats) {
+			if(availableFormat.format == VK_FORMAT_B8G8R8A8_SRGB 
+				&& availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) {
+				return availableFormat;
+			}
+		}
+
+		return availableFormats[0];	// use the first format
+	}
+
+	VkPresentModeKHR chooseSwapPresentMOde(const std::vector<VkPresentModeKHR>& availablePresentModes) {
+		// VK_PRESENT_MODE_IMMEDIATE_KHR: print image immediately, may cause tearing
+		// VK_PRESENT_MODE_FIFO_KHR: put image in a queue, program wait if queue is full, like v-sync, always available
+		// VK_PRESENT_MODE_FIFO_RELAXED_KHR: similar to last one, will not wait if program is late, may cause tearing
+		// VK_PRESENT_MODE_MAILBOX_KHR: similar to FIFO, but will update queue when queue is full 
+		
+		for(const auto& availablePresentMode : availablePresentModes) {
+			if(availablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR) {
+				return availablePresentMode;
+			}
+		}
+
+		return VK_PRESENT_MODE_FIFO_KHR;
+	}
+
 
 	
 	void createLogicalDevice() {
@@ -396,6 +462,8 @@ private:
 
 
 };	// HelloTriangleApplication
+
+
 
 int main() {
 	
