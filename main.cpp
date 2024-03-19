cd9271cd0471de842f6be42264cace98d0b8a59b
ouwenxuan <ouwenxuan@senseauto.com>
2024-03-19 17:41:41 +0800

    finished vertex and fragment shader set up

--- main.cpp
+++ main.cpp
@@ -76,6 +76,7 @@ struct SwapChainSupportDetails {
 };
 
 
+
 class HelloTriangleApplication {
 public:
     void run() {
@@ -593,9 +594,69 @@ private:
 	}
 
 
-	void createGraphicsPipeline() {
+	static std::vector<char> readFile(const std::string& filename) {
+		// start read at the end of file, read as binary
+		std::ifstream file(filename, std::ios::ate | std::ios::binary);
 
+		if(!file.is_open()) {
+			throw std::runtime_error("failed to open file!");
+		}
+
+		size_t fileSize = (size_t) file.tellg();	// read at end, get file size
+		std::vector<char> buffer(fileSize);
+
+		file.seekg(0);
+		file.read(buffer.data(), fileSize);	// read all bytes at once
+
+		file.close();
+		return buffer;
 	}
+
+	VkShaderModule createShaderModule(const std::vector<char>& code) {
+		VkShaderModuleCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+		createInfo.codeSize = code.size();
+		createInfo.pCode = reinterpret_cast<const uint32_t*>(code.data());
+	
+		VkShaderModule shaderModule;
+		if(vkCreateShaderModule(device, &createInfo, nullptr, &shaderModule) != VK_SUCCESS)) {
+			throw std::runtime_error("failed to create shader module!");
+		}
+
+		return shaderModule;
+	}
+
+	void createGraphicsPipeline() {
+		auto vertShaderCode = readFile("shaders/vert.spv");
+		auto fragShaderCode = readFile("shaders/frag.spv");
+	
+		VkShaderModule vertShaderModule = createShaderModule(vertShaderCode);
+		VkShaderModule fragShaderModule = createShaderModule(fragShaderCode);
+
+		// vertex shader stage
+		VkPipelineShaderStageCreateInfo vertShaderStageInfo{};
+		vertShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+		vertShaderStageInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
+		vertShaderStageInfo.module = vertShaderModule;
+		vertShaderStageInfo.pName = "main";
+
+		// fragment shader stag
+		VkPipelineShaderStageCreateInfo fragShaderStageInfo{};
+		fragShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+		fragShaderStageInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+		fragShaderStageInfo.module = fragShaderModule;
+		fragShaderStageInfo.pName = "main";
+
+		VkPipelineShaderStageCreateInfo shaderStages[] = {vertShaderStageInfo, fragShaderStageInfo};
+
+		// clean up
+		vkDestroyShaderModule(device, fragShaderModule, nullptr);
+    	vkDestroyShaderModule(device, vertShaderModule, nullptr);
+	}
+
+
+	
+
 
 };	// HelloTriangleApplication
 
